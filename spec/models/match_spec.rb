08e995e391f33bc4c1b7641e5e65eef66879d599
Markus Olsen <molsen13@gmail.com>
2015-06-24 10:14:53 -0600

    Testing .potential_matches in Match model

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,31 @@
 require "rails_helper"
 
 RSpec.describe Match do
+  let!(:user) do
+    User.create(name: "markus",
+                picture_url: "https://avatars.githubusercontent.com/u/8800860?v=3",
+                description: "pairing is so fun",
+                uid: "8800860",
+                token: "2f569b64641d7834bd99747c97cef30007e8fb29")
+  end
 
+  let!(:user2) do
+    User.create(name: "bob",
+                picture_url: "https://avatars.githubusercontent.com/u/8800860?v=3",
+                description: "pairing is so fun",
+                uid: "1111111",
+                token: "2f569b64641d7834bd99747c97cef30007e8fb29")
+  end
+
+  before(:each) do
+    user.languages.create(name: "ruby")
+  end
+
+  describe ".potential_matches" do
+    it "returns a collection of Users that have clicked 'yes' for the current user" do
+      user.matches.create(match_uid: user2.uid, like: "1")
+
+      expect(Match.potential_matches(user2).first).to eq(user)
+    end
+  end
 end
